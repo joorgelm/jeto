2435ddb7b18ccd10ab9b5d0185cb9ba00f39c6b7
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2023-07-02 15:22:39 -0400

    add check endpoint

--- src/main/java/br/com/joorgelm/jeto/adapter/controller/JetoRestController.java
+++ src/main/java/br/com/joorgelm/jeto/adapter/controller/JetoRestController.java
@@ -6,6 +6,7 @@ import net.sourceforge.tess4j.TesseractException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,11 @@ import java.io.IOException;
 public class JetoRestController {
 
     private ExtractTextUsecase extractTextUsecase;
+
+    @GetMapping
+    public ResponseEntity<String> check() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @PostMapping(value = "/extract")
     public ResponseEntity<String> extract(
